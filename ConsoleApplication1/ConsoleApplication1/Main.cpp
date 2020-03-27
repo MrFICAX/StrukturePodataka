241d7885ecbda84c8259069795bc67d0b02c6cea
17503filiptrajkovic <ficax.al@elfak.rs>
2020-03-27 23:10:28 +0100

    Dodat je test projekat

---
+++ ConsoleApplication1/ConsoleApplication1/Main.cpp
@@ -0,0 +1,9 @@
+#include "Test.h"
+#include <iostream>
+using namespace std;
+
+void  main() {
+	Test* t = new Test();
+	t->Funkcija();
+	delete t;
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Test.cpp
@@ -0,0 +1,7 @@
+#include "Test.h"
+#include <iostream>
+using namespace std;
+
+void Test::Funkcija() {
+	cout << "Ovo je test funkcija";
+}
